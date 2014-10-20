c91bdca68c3d0b0ed9f93cdc69f628663dfdcc9c
wasnot <a.aida@freebit.net>
2014-10-21 02:49:58 +0900

    Initial Commit

---
+++ WifiNameWidget/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WifiNameWidget
+//
+//  Created by AidaAkihiro on 2014/10/21.
+//  Copyright (c) 2014年 Wasnot Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WifiNameWidget/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WifiNameWidget
+//
+//  Created by AidaAkihiro on 2014/10/21.
+//  Copyright (c) 2014年 Wasnot Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
